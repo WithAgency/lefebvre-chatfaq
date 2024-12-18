9c295a13e4e5f623bf786dc2eb4f92e51483e398
rando128 <sthonore@Stephanes-MacBook-Pro-3.local>
2024-12-18 19:17:42 +0100

    Try and fix S3 uploads

--- back/back/config/settings.py
+++ back/back/config/settings.py
@@ -301,6 +301,7 @@ with EnvManager(model_w_django) as env:
         AWS_QUERYSTRING_EXPIRE = "3600"
         AWS_S3_SIGNATURE_VERSION = env.get("AWS_S3_SIGNATURE_VERSION", default=None)
         AWS_S3_REGION_NAME = env.get("AWS_S3_REGION_NAME", default=None)
+        AWS_S3_CUSTOM_DOMAIN = env.get("AWS_S3_CUSTOM_DOMAIN", default=None)
 
     # --------------------------- LLM APIs ---------------------------
     VLLM_ENDPOINT_URL = env.get("VLLM_ENDPOINT_URL", default=None)
