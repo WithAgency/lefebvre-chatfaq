b835d2dcc6509abdd62fdb8996695a0bb1ffa446
rando128 <sthonore@Stephanes-MacBook-Pro-3.local>
2024-12-18 18:26:00 +0100

    Try and fix S3 uploads

--- back/back/config/storage_backends.py
+++ back/back/config/storage_backends.py
@@ -2,6 +2,7 @@ import re
 
 from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
+from botocore.client import Config as BotoConfig  # Import Config with alias to avoid confusion
 from django.core.files.storage import FileSystemStorage
 
 
@@ -32,6 +33,25 @@ class PrivateMediaS3Storage(S3Boto3Storage):
     default_acl = "private"
     file_overwrite = False
     custom_domain = "lefebvre-chatfaq.ams3.digitaloceanspaces.com"  # Set to virtual-hosted domain
+
+    def __init__(self, *args, **kwargs):
+        """
+        Initialize the storage backend with custom configurations for region and addressing style.
+        """
+        # Create a custom Config object with 'virtual' addressing style and 's3v4' signature version
+        boto3_config = BotoConfig(
+            signature_version='s3v4',# self.signature_version,  # Typically 's3v4'
+            addressing_style='virtual'  # Enforce virtual-hosted style URLs
+        )
+
+        # Update the kwargs with the custom domain and config
+        kwargs.update({
+            'custom_domain': self.custom_domain,
+            'config': boto3_config,
+            'region_name': self.region_name,  # Ensure 'region_name' is set in your environment or here
+        })
+
+        super().__init__(*args, **kwargs)
 
     def generate_presigned_url(self, path, content_type, expires_in=3600):
         """
